ebbc84bd952614788439a22667271bdf59eaaa3d
Mikhail Kruglik <miheykrug@mail.ru>
2019-02-04 12:07:18 +0300

    Fix generate_uniq_hash method

--- lib/ext/utils/repo.ex
+++ lib/ext/utils/repo.ex
@@ -5,10 +5,10 @@ defmodule Ext.Utils.Repo do
     {app_name, repo}
   end
 
-  def generate_uniq_hash(schema, column, length \\ 35) do
+  def generate_uniq_hash(schema, column, length \\ 35, repo \\ nil) do
     hash = :crypto.strong_rand_bytes(length) |> Base.url_encode64()
-
-    result = schema |> Arcade.Repo.exists?(%{column => hash})
+    {_, repo} = get_config(repo)
+    result = schema |> repo.exists?(%{column => hash})
 
     cond do
       result -> generate_uniq_hash(schema, column, length)
