b22cf472b9624a777ca516ed6393a1a958d0af35
denislutski <avenger_den@mail.ru>
2019-02-04 17:43:46 +0300

    Fix filter data

--- lib/ext/gql/resolvers/base.ex
+++ lib/ext/gql/resolvers/base.ex
@@ -24,7 +24,7 @@ defmodule Ext.Gql.Resolvers.Base do
     fn args, _ ->
       page_limit = args[:limit] || Application.get_env(app_name, :page_limit) || 20
       offset = args[:offset] || 0
-      filter = Jason.decode!(args[:filter] || "{}") |> Ext.Utils.Base.snake_keys()
+      filter = if args[:filter], do: Ext.Utils.Base.snake_keys(args[:filter]), else: %{}
       try do
         entities =
           schema
