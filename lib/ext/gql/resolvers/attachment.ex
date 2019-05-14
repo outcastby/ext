29926c6fccb2ab2ba12b6e1c9b548721dba7eb1f
Mikhail Kruglik <miheykrug@mail.ru>
2019-05-14 13:07:38 +0300

    Add version arg for Attachment resolver
    https://trulysocialgames.atlassian.net/browse/GM-000

--- lib/ext/gql/resolvers/attachment.ex
+++ lib/ext/gql/resolvers/attachment.ex
@@ -1,9 +1,9 @@
 defmodule Ext.Gql.Resolvers.Attachment do
   @moduledoc false
 
-  def url(uploader, version \\ nil) do
-    fn parent, _args, %{definition: %{schema_node: %{identifier: field_name}}} ->
-      {:ok, uploader.url({Ext.Utils.Base.get_in(parent, [field_name]), parent}, version, signed: true)}
+  def url(uploader) do
+    fn parent, args, %{definition: %{schema_node: %{identifier: field_name}}} ->
+      {:ok, uploader.url({Ext.Utils.Base.get_in(parent, [field_name]), parent}, args[:version], signed: true)}
     end
   end
 end
