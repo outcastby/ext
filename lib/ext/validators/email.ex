baf497c4460227dba144ea44d632801072c7d12c
Mikhail Kruglik <miheykrug@mail.ru>
2019-05-31 13:11:43 +0300

    Change email validator

--- lib/ext/validators/email.ex
+++ lib/ext/validators/email.ex
@@ -5,10 +5,9 @@ defmodule Ext.Validators.Email do
   def call(form, args) do
     {field, message} = parse_args(args)
 
-    case Regex.run(~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, Map.get(form.changes, field)) do
-      nil -> form |> add_error(field, message)
-      _ -> form
-    end
+    if Regex.match?(~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, Map.get(form.changes, field)),
+      do: form,
+      else: form |> add_error(field, message)
   end
 
   defp parse_args(args), do: {args[:field], args[:message] || "invalid_email"}
