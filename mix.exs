99b628192350199ef832a4515bb4c62db2c8727c
Mikhail Kruglik <miheykrug@mail.ru>
2019-07-05 15:23:59 +0300

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ext.MixProject do
     [
       app: :ext,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
