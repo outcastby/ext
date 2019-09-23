8e789f70c571db5427cce83207f18e3a703dd724
Mikhail Kruglik <miheykrug@mail.ru>
2019-09-23 15:56:43 +0300

    Add RunInitializers

---
+++ lib/ext/core/run_initializers.ex
@@ -0,0 +1,10 @@
+defmodule RunInitializers do
+  @moduledoc false
+
+  def call(app_name) do
+    for initializer <- Path.wildcard("lib/#{app_name}/initializers/*.ex") do
+      String.to_atom("Elixir.Initializers.#{initializer |> Path.basename(".ex") |> Macro.camelize()}")
+      |> apply(:call, [])
+    end
+  end
+end
