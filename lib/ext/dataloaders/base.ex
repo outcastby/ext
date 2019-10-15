e6555401de545f4f10d9ee6f12129ac053690f9b
Mikhail Kruglik <miheykrug@mail.ru>
2019-10-15 22:22:21 +0300

    Add order_by and limit to dataloader

--- lib/ext/dataloaders/base.ex
+++ lib/ext/dataloaders/base.ex
@@ -1,11 +1,22 @@
 defmodule Ext.Dataloaders.Base do
+  import Ecto.Query
+
   def data(repo) do
     Dataloader.Ecto.new(repo, query: &data_query(&1, &2, repo), run_batch: &run_batch(&1, &2, &3, &4, &5, repo))
   end
 
   def data_query(queryable, params, repo) do
-    queryable |> repo.where(params)
+    queryable |> handle_filter(params, repo) |> handle_order_by(params) |> handle_limit(params)
   end
+
+  defp handle_filter(query, %{filter: filter}, repo), do: repo.where(query, filter)
+  defp handle_filter(query, _, _), do: query
+
+  defp handle_order_by(query, %{order_by: order_by}), do: order_by(query, ^order_by)
+  defp handle_order_by(query, _), do: query
+
+  defp handle_limit(query, %{limit: count}), do: limit(query, ^count)
+  defp handle_limit(query, _), do: query
 
   def run_batch(queryable, query, col, inputs, repo_opts, repo) do
     queryable_name = queryable |> Atom.to_string() |> String.replace(~r/Elixir./, "")
