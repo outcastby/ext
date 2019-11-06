5d9169270f7595167586e60a17b9d59eb9108a3f
denislutski <avenger_den@mail.ru>
2019-11-06 15:50:11 +0300

    Revert logger configs

--- lib/ext/logger/config.exs
+++ lib/ext/logger/config.exs
@@ -1,51 +1,61 @@
 use Mix.Config
 
+file_backends =
+  cond do
+    System.get_env("LOG_LEVEL") == "debug" ->
+      [
+        {LoggerFileBackend, :error_log},
+        {LoggerFileBackend, :warn_log},
+        {LoggerFileBackend, :info_log},
+        {LoggerFileBackend, :debug_log}
+      ]
+
+    System.get_env("LOG_LEVEL") == "info" ->
+      [{LoggerFileBackend, :error_log}, {LoggerFileBackend, :warn_log}, {LoggerFileBackend, :info_log}]
+
+    System.get_env("LOG_LEVEL") == "warn" ->
+      [{LoggerFileBackend, :error_log}, {LoggerFileBackend, :warn_log}]
+
+    System.get_env("LOG_LEVEL") == "error" ->
+      [{LoggerFileBackend, :error_log}]
+
+    true ->
+      []
+  end
+
 metadata = [:request_id, :sdk, :method, :url]
 format = "$date $time $metadata[$level] $message\n"
 
 config :logger,
   format: format,
-  handle_otp_reports: false,
-  backends: [:console, LoggerLagerBackend, {Ext.Logger.Rollbar.ErrorSend, :error_log}]
+  backends:
+    [:console | file_backends] ++
+      if(Code.ensure_compiled?(Rollbax), do: [{Ext.Logger.Rollbar.ErrorSend, :error_log}], else: [])
+
+config :logger, :error_log,
+  path: "log/error.log",
+  format: format,
+  metadata: metadata,
+  level: :error
+
+config :logger, :warn_log,
+  path: "log/warn.log",
+  format: format,
+  metadata: metadata,
+  level: :warn
+
+config :logger, :info_log,
+  path: "log/info.log",
+  format: format,
+  metadata: metadata,
+  level: :info
+
+config :logger, :debug_log,
+  path: "log/debug.log",
+  format: format,
+  metadata: metadata,
+  level: :debug
 
 config :logger, :console,
   format: format,
   metadata: metadata
-
-config :lager,
-  handlers: [
-    lager_file_backend: [
-      file: 'log/debug.log',
-      level: :debug,
-      size: 10_485_760,
-      date: '$D0',
-      count: 2
-    ],
-    lager_file_backend: [
-      file: 'log/info.log',
-      level: :info,
-      size: 10_485_760,
-      date: '$D0',
-      count: 2
-    ],
-    lager_file_backend: [
-      file: 'log/warn.log',
-      level: :warning,
-      size: 10_485_760,
-      date: '$D0',
-      count: 2
-    ],
-    lager_file_backend: [
-      file: 'log/error.log',
-      level: :error,
-      size: 10_485_760,
-      date: '$D0',
-      count: 2
-    ]
-  ],
-  colored: true,
-  crash_log: 'log/crash.log',
-  crash_log_msg_size: 65536,
-  crash_log_size: 10_485_760,
-  crash_log_date: '$D0',
-  crash_log_count: 2
