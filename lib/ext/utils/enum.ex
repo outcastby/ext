51b46b660233fd54ea8d0c88101af088b9c226d0
denislutski <avenger_den@mail.ru>
2019-11-21 18:28:12 +0300

    Add randomize items by key

--- lib/ext/utils/enum.ex
+++ lib/ext/utils/enum.ex
@@ -14,10 +14,36 @@ defmodule Ext.Utils.Enum do
     iex> Ext.Utils.Enum.sum_by([%{name: "one", value: 1}, %{name: "two", value: 2}], &(&1.value))
     3
   """
-
   def sum_by(enum, callback) do
     Enum.reduce(enum, 0, fn item, sum ->
       sum + callback.(item)
     end)
+  end
+
+  @doc """
+  Randomize item by key weight
+
+  ## Examples
+    iex> Ext.Utils.Enum.randomize_by([], :key)
+    nil
+
+    iex> Ext.Utils.Enum.randomize_by([%{key: 100}], :key)
+    %{key: 100}
+
+    iex> Ext.Utils.Enum.randomize_by([%{key: 1_000_000}, %{key: 1}], :key)
+    %{key: 1_000_000}
+  """
+  def randomize_by(items, key) do
+    total_weight = items |> Enum.map(&Map.get(&1, key)) |> Enum.sum()
+
+    {items, _} =
+      Enum.reduce(items, {[], 0}, fn item, {resulted_items, range_from} ->
+        range_to = range_from + Map.get(item, key) / total_weight
+        item = %{item: item, range_from: range_from, range_to: range_to}
+        {[item | resulted_items], range_to}
+      end)
+
+    random_value = :rand.uniform()
+    items |> Enum.find(&(&1.range_from <= random_value && &1.range_to >= random_value)) |> get_in([:item])
   end
 end
