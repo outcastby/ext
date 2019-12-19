f0411a233c6e517355a91a3216d37d0ac1c150f6
denislutski <avenger_den@mail.ru>
2019-12-19 16:59:30 +0300

    Enhance Ext.Config

--- lib/ext/config.ex
+++ lib/ext/config.ex
@@ -1,14 +1,28 @@
 defmodule Ext.Config do
-  def get(path, default_value \\ nil)
+  defmacro __using__(opts \\ []) do
+    caller = Keyword.get(opts, :otp_app)
 
-  def get(path, default_value) when is_atom(path),
-    do: Application.get_env(Mix.Project.config()[:app], path, default_value || [])
+    quote do
+      def get(path, default_value \\ nil)
+      def get(path, default_value) when is_atom(path), do: Ext.Config.get(unquote(caller), path, default_value)
+
+      def get(path, default_value) do
+        [head | tail] = path
+        unquote(caller) |> Ext.Config.get(head, nil) |> Ext.Config.get_in_object(tail) || default_value
+      end
+    end
+  end
+
+  def get(path, default_value \\ nil)
+  def get(path, default_value) when is_atom(path), do: get(Mix.Project.config()[:app], path, default_value)
 
   def get(path, default_value) do
     [head | tail] = path
     head |> get() |> get_in_object(tail) || default_value
   end
 
-  defp get_in_object(map, []), do: map
-  defp get_in_object(map, path), do: get_in(map, path)
+  def get(app, path, default_value), do: Application.get_env(app, path, default_value || [])
+
+  def get_in_object(map, []), do: map
+  def get_in_object(map, path), do: get_in(map, path)
 end
