077c1a1cdc56afb489a942efc5c169d5ab2c3827
denislutski <avenger_den@mail.ru>
2020-01-27 17:46:10 +0300

    Fix json_logger

--- lib/ext/logger/json.ex
+++ lib/ext/logger/json.ex
@@ -1,19 +1,22 @@
 defmodule Ext.Logger.Json do
   import Ext.Utils.Map
+  alias Ext.Logger.Json.TimestampFormatter
+  # Fork from https://github.com/soundtrackyourbrand/exlogger
 
+  @spec format(
+          Logger.level(),
+          Logger.message(),
+          Logger.Formatter.time(),
+          Logger.Formatter.keyword()
+        ) :: IO.chardata()
   def format(level, message, timestamp, metadata) do
-    try do
-      time =
-        case Timex.format(timestamp, "{ISO:Extended:Z}") do
-          {:ok, time_str} -> time_str
-          _ -> Timex.format!(Timex.now(), "{ISO:Extended:Z}")
-        end
+    offset = :logger |> Application.fetch_env!(:utc_log) |> TimestampFormatter.utc_offset()
+    time = TimestampFormatter.format(timestamp, offset)
 
-      log_data = %{"msg" => normalize_msg(message), "level" => level, "ts" => time} ||| Map.new(metadata)
-      "#{Jason.encode!(log_data)}\n"
-    rescue
-      _ -> "#{timestamp} #{metadata[level]} #{level} #{normalize_msg(message)}\n"
-    end
+    log_data = %{"msg" => normalize_msg(message), "level" => level, "ts" => time} ||| Map.new(metadata)
+    "#{Jason.encode!(log_data)}\n"
+  rescue
+    _ -> "#{TimestampFormatter.format(timestamp, {0, 0})} #{metadata[level]} #{level} #{normalize_msg(message)}\n"
   end
 
   defp normalize_msg(msg) when is_binary(msg), do: msg
